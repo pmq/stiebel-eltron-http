d34993b22753eb5798396d5a9190611156420f8b
Pierre Queinnec <pierre@queinnec.org>
2025-08-28 08:54:08 +0000

    Use the MAC address as unique ID for the ISG device config flows

--- custom_components/stiebel_eltron_http/config_flow.py
+++ custom_components/stiebel_eltron_http/config_flow.py
@@ -7,7 +7,7 @@ from urllib.parse import urlsplit
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_NAME
+from homeassistant.const import CONF_DEVICE_ID, CONF_HOST, CONF_NAME
 
 if TYPE_CHECKING:
     from homeassistant.config_entries import ConfigFlowResult
@@ -22,7 +22,7 @@ from homeassistant.helpers.service_info.ssdp import (
 )
 from slugify import slugify
 
-from .const import DOMAIN, LOGGER
+from .const import DOMAIN, LOGGER, MAC_ADDRESS_KEY
 from .scrapper import (
     StiebelEltronScrapingClient,
     StiebelEltronScrapingClientAuthenticationError,
@@ -44,12 +44,29 @@ class StiebelEltronIsgHttpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         _errors = {}
 
         if user_input is not None:
+            _default_host = user_input[CONF_HOST]
             self.config = {
                 CONF_HOST: user_input[CONF_HOST],
             }
 
             try:
+                # try to connect and verify that it looks like a Stiebel Eltron ISG
                 await self._test_connect(host=self.config[CONF_HOST])
+
+                # retrieve the MAC address from the device
+                self.config[CONF_DEVICE_ID] = await self._get_mac_address(
+                    host=self.config[CONF_HOST]
+                )
+
+                LOGGER.debug("Discovered device with config: %s", self.config)
+
+                # set a unique ID based on the MAC address
+                await self._format_and_set_unique_id(self.config[CONF_DEVICE_ID])
+
+                return self.async_create_entry(
+                    title=self.config[CONF_HOST],
+                    data=user_input,
+                )
 
             except StiebelEltronScrapingClientAuthenticationError as exception:
                 LOGGER.warning(exception)
@@ -61,26 +78,13 @@ class StiebelEltronIsgHttpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 LOGGER.exception(exception)
                 _errors["base"] = "unknown"
 
-            else:
-                ## Do NOT use this in production code (PMQ: use the MAC address)
-                ## The unique_id should never be something that can change
-                ## https://developers.home-assistant.io/docs/config_entries_config_flow_handler#unique-ids
-                unique_id = slugify(user_input[CONF_HOST])
-                LOGGER.debug("Setting unique_id to %s", unique_id)
-
-                await self.async_set_unique_id(unique_id)
-                self._abort_if_unique_id_configured()
-
-                return self.async_create_entry(
-                    title=user_input[CONF_HOST],
-                    data=user_input,
-                )
+        _default_host = self.config[CONF_HOST] if hasattr(self, "config") else ""
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_HOST, default=self.config[CONF_HOST]): str,
+                    vol.Required(CONF_HOST, default=_default_host): str,
                 },
             ),
             errors=_errors,
@@ -90,10 +94,10 @@ class StiebelEltronIsgHttpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self, discovery_info: SsdpServiceInfo
     ) -> ConfigFlowResult:
         """Prepare configuration for a SSDP discovered device."""
-        LOGGER.info("Discovered device with UPnP info: %s", discovery_info.upnp)
+        LOGGER.info("Discovered SSDP device with UPnP info: %s", discovery_info.upnp)
         url = urlsplit(discovery_info.upnp[ATTR_UPNP_PRESENTATION_URL])
         mac_address = format_mac(discovery_info.upnp[ATTR_UPNP_SERIAL])
-        LOGGER.debug("MAC address: %s", mac_address)
+        LOGGER.debug("Found MAC address from UPnP: %s", mac_address)
 
         self.config = {
             CONF_HOST: url.hostname,
@@ -101,8 +105,8 @@ class StiebelEltronIsgHttpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         self._async_abort_entries_match({CONF_HOST: self.config[CONF_HOST]})
 
-        await self.async_set_unique_id(mac_address)
-        self._abort_if_unique_id_configured(updates=self.config)
+        # set a unique ID based on the MAC address
+        await self._format_and_set_unique_id(mac_address)
 
         self.context["title_placeholders"] = {
             CONF_NAME: discovery_info.upnp[ATTR_UPNP_FRIENDLY_NAME],
@@ -118,3 +122,29 @@ class StiebelEltronIsgHttpFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             session=async_create_clientsession(self.hass),
         )
         await client.async_test_connect()
+
+    async def _get_mac_address(self, host: str) -> str:
+        """Retrieve the MAC address from the ISG."""
+        client = StiebelEltronScrapingClient(
+            host=host,
+            session=async_create_clientsession(self.hass),
+        )
+        mac_address_result = await client.async_get_mac_address()
+
+        if not mac_address_result:
+            msg = "Could not retrieve MAC address"
+            raise StiebelEltronScrapingClientError(msg)
+
+        mac_address = mac_address_result.get(MAC_ADDRESS_KEY)
+        LOGGER.debug("Found MAC address from ISG: %s", mac_address)
+
+        return mac_address
+
+    async def _format_and_set_unique_id(self, mac_address: str) -> None:
+        """Format the MAC address and use it for unique ID."""
+        _unique_id = slugify(mac_address)
+        LOGGER.debug(
+            "Formatting MAC address %s into unique ID %s", mac_address, _unique_id
+        )
+        await self.async_set_unique_id(_unique_id)
+        self._abort_if_unique_id_configured(updates=self.config)
